71644258801d93da221eb6cc8cebfef0b47755ee
Chao Li <104707579+ChaoLiAtRIKEN@users.noreply.github.com>
2024-02-26 17:20:25 +0900

    Update preliminares.py

--- preliminares.py
+++ preliminares.py
@@ -10,13 +10,20 @@ def normalize_to_one(u):
     """
     return np.abs(u) / np.sum(np.abs(u))
 
+@jax.jit
 def log_tsallis(x, alpha):
+    """
+    The Tsallis Logrithm function required in the formula
+    """
     if alpha != 0:
         return (x ** alpha - 1)/alpha
     else:
         return np.log(x)
     
 def exp_tsallis(x, alpha):
+    """
+    The Tsallis exponent function, which is the inverse of Tsallis logirhtm function
+    """
     if alpha != 0:
         return np.where(1+alpha*x<=0, 0 ** (1/alpha), (1+alpha*x) ** (1/alpha))
     else:
@@ -39,16 +46,34 @@ def loss_fn(theta, x):
     estimation_data = model(theta)
     return jnp.sum(jnp.square(x - estimation_data))
 
-def grad_loss(theta, x, ab):
+def grad_loss(theta, x):
+    """
+    Return the gradient of the loss function, wrt. the two factors
+    """
     grad_U, grad_V = jax.grad(loss_fn)(theta, x)
+    return grad_U, grad_V
 
 
 def update(theta, x, ab, lr=0.1):
-    pass
+    alpha, beta = ab
+    grad_U, grad_V = grad_loss(theta, x)
+    U, V = theta
+    U_new = U * exp_tsallis((-lr/U ** (alpha+beta-1)) * grad_U, beta)
+    V_new = V * exp_tsallis((-lr/V ** (alpha+beta-1)) * grad_V, beta)
+
+    return U_new/jnp.sum(U_new), V_new/jnp.sum(V_new)
 
 
 
 # 3. Evaluation
-# print(np.sum(U[1]))
+ab = (1.0, 0.0)
+theta = [normalize_to_one(np.random.normal(0,1,size=[i, rank_factors])) for i in size_data]
 
-print(exp_tsallis(log_tsallis(3,0),0))
+for _ in range(1000):
+    theta = update(theta, data, ab, lr=0.3)
+    loss = loss_fn(theta, data)
+    print('Loss: ', loss)
+
+print(U[0])
+
+# print(exp_tsallis(log_tsallis(U[0], 2), 2))
